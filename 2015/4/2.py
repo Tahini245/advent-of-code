eb4e504ed6d0e35d8bf04e30a186eaf8331d1d5b
Tahini245 <taheenvpp@gmail.com>
2021-10-27 12:26:25 +0100

    Answer day 4

---
+++ 2015/4/2.py
@@ -0,0 +1,12 @@
+from hashlib import md5
+
+
+def main(input: str) -> int:
+    x = 1
+    while not md5((input + str(x)).encode()).hexdigest().startswith('000000'):
+        x += 1
+    return x
+
+
+if __name__ == '__main__':
+    print('The lowest number to produce such a hash is', main('ckczppom'))
